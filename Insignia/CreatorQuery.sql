2e37a04b473cb2566181ced5cf637baf41a40b88
Marc d'Avernas <marc_davernas@wrdsb.on.ca>
2014-07-08 08:09:36 -0400

    july 8th update

--- Insignia/CreatorQuery.sql
+++ Insignia/CreatorQuery.sql
@@ -1,90 +1,72 @@
 --Pivot report
+go
+use ils
 select * from (
 select 'Items created' as type, t02CreatedBy as name, count(t02CreatedBy) as number
 from dbo.Items_t02
-where t02DateCreated > getdate() - 7
+where t02DateCreated > getdate() - 6
 group by t02CreatedBy
 Union All
 select 'Items modified' as type, t02ModifiedBy as name, count(t02ModifiedBy) as number
 from dbo.Items_t02
-where t02DateModified > getdate() - 7
+where t02DateModified > getdate() - 6
 group by t02ModifiedBy
 Union All
 select 'Copies created' as type, t03CreatedBy as name, count(t03CreatedBy) as number
 from dbo.Copies_t03
-where t03DateCreated > getdate() - 7
+where t03DateCreated > getdate() - 6
 group by t03CreatedBy
 Union All
 select 'Copies modified' as type, t03ModifiedBy as name, count(t03ModifiedBy) as number
 from dbo.Copies_t03
-where t03DateModified > getdate() - 7
+where t03DateModified > getdate() - 6
 group by t03ModifiedBy
 ) as query
 
 --Insignia Items
 select t02CreatedBy, t02DateCreated as create_date
 from dbo.Items_t02
-where t02DateCreated > getdate() - 7
+where t02DateCreated > getdate() - 6
 order by t02CreatedBy
 
 select t02ModifiedBy, CONVERT(date,t02DateModified) as modified_date
 from dbo.Items_t02
-where t02DateModified > getdate() - 7
+where t02DateModified > getdate() - 6
 order by t02ModifiedBy
 
 --Insignia Copies
 select t03CreatedBy, CONVERT(date,t03DateCreated) as create_date
 from dbo.Copies_t03
-where t03DateCreated > getdate() - 7
+where t03DateCreated > getdate() - 6
 order by t03CreatedBy
 
 select t03ModifiedBy, CONVERT(date,t03DateModified) as modified_date
 from dbo.Copies_t03
-where t03DateModified > getdate() - 7
+where t03DateModified > getdate() - 6
 order by t03ModifiedBy
 
 --reports
 select t02CreatedBy as name, count(t02CreatedBy) as created
 from dbo.Items_t02
-where t02DateCreated > getdate() - 7
+where t02DateCreated > getdate() - 6
 group by t02CreatedBy
 
 select t02ModifiedBy as name, count(t02ModifiedBy) as modified
 from dbo.Items_t02
-where t02DateModified > getdate() - 7
+where t02DateModified > getdate() - 6
 group by t02ModifiedBy
 
 select t03CreatedBy as name, count(t03CreatedBy) as created
 from dbo.Copies_t03
-where t03DateCreated > getdate() - 7
+where t03DateCreated > getdate() - 6
 group by t03CreatedBy
 
 select t03ModifiedBy as name, count(t03ModifiedBy) as modified
 from dbo.Copies_t03
-where t03DateModified > getdate() - 7
+where t03DateModified > getdate() - 6
 group by t03ModifiedBy
 
 --Past reports
-select t02CreatedBy as name, count(t02CreatedBy) as created
-from dbo.Items_t02
-where t02DateCreated > getdate() - 7
-group by t02CreatedBy
-
-select t02ModifiedBy as name, count(t02ModifiedBy) as modified
-from dbo.Items_t02
-where t02DateModified > getdate() - 7
-group by t02ModifiedBy
-
-select t03CreatedBy as name, count(t03CreatedBy) as created
-from dbo.Copies_t03
-where t03DateCreated > getdate() - 7
-group by t03CreatedBy
-
-select t03ModifiedBy as name, count(t03ModifiedBy) as modified
-from dbo.Copies_t03
-where t03DateModified > getdate() - 7
-group by t03ModifiedBy
-
 --First week pivot
 select * from (
 select 'Items created' as type, t02CreatedBy as name, count(t02CreatedBy) as number
